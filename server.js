8c8c5b070a5601c6a98a17405b50c010da0704fa
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-12 15:24:54 +0530

    MongoDB connection setup

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const db = require("./db.js")
+const db = require("./db.js");
 
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('Server is working 🔥')
-})
+app.get("/", (req, res) => {
+  res.send("Server is working 🔥");
+});
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => `Server running on port port 🔥`)
+app.listen(port, () => `Server running on port port 🔥`);
