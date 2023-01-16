737bc6e9489a735cc5a11cfd474554a03f9b5134
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-16 17:41:58 +0530

    nothing

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ app.use('/api/pizzas/', pizzasRoute)
 app.use('/api/users/' , userRoute)
 app.use('/api/orders/' , ordersRoute)
 
-// app.get("/", (req, res) => {
-//   res.send("Server is working 🔥");
-// });
+app.get("/", (req, res) => {
+  res.send("Server is working 🔥");
+});
 
-if(process.env.NODE_ENV ==='production')
-{
-    app.use('/' , express.static('client/build'))
+// if(process.env.NODE_ENV ==='production')
+// {
+    app.use('/' , express.static('./client/build'))
 
     app.get('*' , (req , res)=>{
 
-        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname  , './client/build/index.html'))
 
     })
-}
+// }
 
 const port = process.env.PORT || 8000;
 
