6b2995324adccc6a022c3db307023045c5e684f6
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-17 12:25:10 +0530

    deploy for fortend

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 require("dotenv").config();
 const express = require("express");
-const Pizza = require('./models/pizzaModel')
+const Pizza = require("./models/pizzaModel");
 
 const app = express();
 const db = require("./db.js");
-const path = require('path')
+const path = require("path");
 
 app.use(express.json());
-const pizzasRoute = require('./routes/pizzasRoute')
-const userRoute = require('./routes/userRoute')
-const ordersRoute = require('./routes/ordersRoute')
+const pizzasRoute = require("./routes/pizzasRoute");
+const userRoute = require("./routes/userRoute");
+const ordersRoute = require("./routes/ordersRoute");
 
-
-app.use('/api/pizzas/', pizzasRoute)
-app.use('/api/users/' , userRoute)
-app.use('/api/orders/' , ordersRoute)
+app.use("/api/pizzas/", pizzasRoute);
+app.use("/api/users/", userRoute);
+app.use("/api/orders/", ordersRoute);
 
 app.get("/", (req, res) => {
   res.send("Server is working 🔥");
 });
 
-// if(process.env.NODE_ENV ==='production')
-// {
-//     app.use('/' , express.static('client/build'))
+app.use("/", express.static("client/build"));
 
-//     app.get('*' , (req , res)=>{
-
-//         res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
-
-//     })
-// }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+});
 
 const port = process.env.PORT || 8000;
 
